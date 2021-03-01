0e5e4a172235c5ac549a6256bca41e0b464a684a
Kushagra Kumar <kushagraagent47@gmail.com>
2021-03-02 03:58:53 +0530

    Movie List App

---
+++ movieapp/src/components/MovieForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form @submit.prevent="onSubmit" class="text-center">
+    <input
+      type="text"
+      class="form-control mb-2"
+      name="name"
+      v-model="name"
+      placeholder="Awesome Movie Name"
+    />
+    <input
+      type="text"
+      class="form-control mb-2"
+      name="url"
+      v-model="url"
+      placeholder="Awesome Movie Url"
+    />
+    <button class="btn btn-info btn-block" type="submit">Add</button>
+  </form>
+</template>
+
+<script>
+import { v4 as uuidv4 } from "uuid";
+
+export default {
+  name: "Form",
+  props: {
+    addMovie: Function,
+  },
+  data() {
+    return {
+      name: "",
+      url: "",
+    };
+  },
+  methods: {
+    onSubmit: function() {
+      this.addMovie({
+        name: this.name,
+        url: this.url,
+        id: uuidv4(),
+        date: Date.now(),
+      });
+
+      this.name = "";
+      this.url = "";
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ movieapp/src/layout/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary">
+    <a class="navbar-brand ml-5">
+      <img src="../assets/logo-white.png" width="30" height="30" />
+    </a>
+    <span class="navbar-text text-white">
+      {{ title }}
+    </span>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ["title"],
+};
+</script>
+
+<style></style>
